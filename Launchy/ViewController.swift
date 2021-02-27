3ea5c0530fe4c88dfb79e8d3268118559de91f71
A Jauzee <a.jauzee@gmail.com>
2021-02-27 13:34:49 +0300

    Initial Commit

---
+++ Launchy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//
+//  Launchy
+//  
+//  Created on 27.02.2021
+//  
+//  
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
