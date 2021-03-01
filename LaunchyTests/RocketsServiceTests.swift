146fd0fbd9daed3893a269e56968f543dcf08c07
A Jauzee <a.jauzee@gmail.com>
2021-03-01 10:37:57 +0300

    Write basic tests for RocketsService

--- LaunchyTests/RocketsServiceTests.swift
+++ LaunchyTests/RocketsServiceTests.swift
@@ -7,27 +7,26 @@
 //  
 
 import XCTest
+@testable import Launchy
 
 class RocketsServiceTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    var networkService: MockNetworkService!
+    var rocketsService: RocketsService!
+
+    override func setUp() {
+        networkService = MockNetworkService()
+        rocketsService = RocketsServiceProvider.makeRocketsService(networkService: networkService)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+    func testRequestConfigHasCorrectParams() throws {
+        let id = UUID().uuidString
+        _ = rocketsService.getRocket(id: id)
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+        let requestConfig = try XCTUnwrap(networkService.requestConfig)
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+        XCTAssertEqual(try requestConfig.url.asURL().absoluteString, "https://api.spacexdata.com/v4/rockets/\(id)")
+        XCTAssertEqual(requestConfig.method.rawValue, "GET")
     }
 
 }
